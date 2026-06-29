80df9b4e7fe1e6bcc12c0552982cb648d20d443d
kathukyabrian <bkitunda@meliora.tech>
2026-06-29 12:09:30 +0300

    used lombok annotations in MulikaHTTPResponse to remove boilerplate getters, setters and constructors

--- src/main/java/tech/meliora/mulika/http/MulikaHTTPResponse.java
+++ src/main/java/tech/meliora/mulika/http/MulikaHTTPResponse.java
@@ -1,42 +1,14 @@
 package tech.meliora.mulika.http;
 
-/**
- *
- * @author kamochu
- */
-public class MulikaHTTPResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MulikaHTTPResponse {
     private int responseCode;
     private String body;
-
-    public MulikaHTTPResponse() {
-    }
-
-    public MulikaHTTPResponse(int responseCode, String body) {
-        this.responseCode = responseCode;
-        this.body = body;
-    }
-
-    public int getResponseCode() {
-        return responseCode;
-    }
-
-    public void setResponseCode(int responseCode) {
-        this.responseCode = responseCode;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    @Override
-    public String toString() {
-        return "HTTPResponse{" + "responseCode=" + responseCode + ", body=" + body + '}';
-    }
-
 }
  
